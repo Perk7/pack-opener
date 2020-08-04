0cae079004036493a6eeb88873d7534bc4910dba
Perk7 <denisperkov0@gmail.com>
2020-08-04 22:42:19 +0700

    My Django Girls app, first commit

--- packs/models.py
+++ packs/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime    
+from decimal import Decimal
+from django.conf import settings
+from django.utils import timezone
+from copy import deepcopy
+from django.shortcuts import get_object_or_404
 
-# Create your models here.
+# Паки
+
+class Pack(models.Model):
+
+	name = models.CharField(max_length=16)
+	prescript = models.TextField(max_length=105)
+	cost = models.IntegerField()
+	icon = models.ImageField(upload_to='packs', height_field=None, width_field=None, max_length=100)
+
+	pubdate = models.DateTimeField(default=datetime.now())
+
+	class Meta:
+	    verbose_name = 'Пак'
+	    verbose_name_plural = 'Паки'
+
+# Карточки
+
+class Card(models.Model):
+	name = models.CharField(max_length=16)
+	quicksell = models.IntegerField()
+	image = models.ImageField(upload_to='cards', height_field=None, width_field=None, max_length=100)
+	chance = models.IntegerField()
+
+	pubdate = models.DateTimeField(default=datetime.now())
+
+	class Meta:
+	    verbose_name = 'Карточка'
+	    verbose_name_plural = 'Карточки'
