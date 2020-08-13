0e3bcfd63608db27721e6b6e4828dab382858748
Perk7 <denisperkov0@gmail.com>
2020-08-13 14:20:47 +0700

    My Django Girls app, first commit

--- pack_opener/settings.py
+++ pack_opener/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticdirs'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
