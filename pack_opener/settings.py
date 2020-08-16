a0f75a31659c09100abb53cb41a35dddc498407d
Perk7 <denisperkov0@gmail.com>
2020-08-16 14:12:34 +0700

    12312

--- pack_opener/settings.py
+++ pack_opener/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 
